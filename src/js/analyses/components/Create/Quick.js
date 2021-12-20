f8d4ef9f8833ba485793611271b9a25ff064655a
reecehoffmann <reecehoffmann@outlook.com>
2021-12-20 12:15:27 -0800

    chore: migrate quick analysis logic to formik

--- src/js/analyses/components/Create/Quick.js
+++ src/js/analyses/components/Create/Quick.js
@@ -1,10 +1,21 @@
-import { forEach, get } from "lodash-es";
+import { forEach } from "lodash-es";
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import styled from "styled-components";
 import { getAccountId } from "../../../account/selectors";
 import { pushState } from "../../../app/actions";
-import { Badge, Button, Icon, Modal, ModalBody, ModalFooter, ModalHeader, ModalTabs, TabLink } from "../../../base";
+import {
+    Badge,
+    Button,
+    Icon,
+    Modal,
+    ModalBody,
+    ModalFooter,
+    ModalHeader,
+    ModalTabs,
+    TabLink,
+    InputError
+} from "../../../base";
 import { deselectSamples } from "../../../samples/actions";
 import { getSelectedSamples } from "../../../samples/selectors";
 import { shortlistSubtractions } from "../../../subtraction/actions";
@@ -16,12 +27,13 @@ import {
     getQuickAnalysisMode
 } from "../../selectors";
 import HMMAlert from "../HMMAlert";
-import { useCreateAnalysis } from "./hooks";
 import { ReferenceSelector } from "./ReferenceSelector";
 import { SelectedSamples } from "./SelectedSamples";
 import { SubtractionSelector } from "./SubtractionSelector";
 import { CreateAnalysisSummary } from "./Summary";
 import { WorkflowSelector } from "./WorkflowSelector";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
 
 const QuickAnalyzeFooter = styled(ModalFooter)`
     align-items: center;
@@ -33,6 +45,22 @@ const QuickAnalyzeSelected = styled.span`
     align-self: center;
     margin: 0 15px 0 auto;
 `;
+
+const QuickAnalyzeError = styled(InputError)`
+    margin: -5px 0px 0px;
+`;
+
+const initialValues = {
+    workflows: [],
+    subtractions: [],
+    references: []
+};
+
+const validationSchema = Yup.object().shape({
+    workflows: Yup.array().min(1, "At least one workflow must be selected"),
+    subtractions: Yup.array().min(1, "At least one subtraction must be selected"),
+    references: Yup.array().min(1, "At least one reference must be selected")
+});
 
 export const QuickAnalyze = ({
     accountId,
@@ -63,34 +91,12 @@ export const QuickAnalyze = ({
     }, [mode]);
 
     // Use this as the subtraction if none is selected.
-    const firstSubtractionId = get(subtractionOptions, [0, "id"]);
+    // const firstSubtractionId = get(subtractionOptions, [0, "id"]);
 
-    const handleSubmit = e => {
-        e.preventDefault();
-
-        if (!references.length) {
-            return setError("Please select reference(s)");
-        }
-
-        if (!workflows.length) {
-            return setError("Please select workflow(s)");
-        }
-
-        onAnalyze(compatibleSamples, references, subtractions || firstSubtractionId, accountId, workflows);
+    const handleSubmit = ({ references, subtractions, workflows }) => {
+        onAnalyze(compatibleSamples, references, subtractions, accountId, workflows);
         onUnselect(compatibleSamples.map(sample => sample.id));
     };
-
-    // This hook is shared with the analyze modal for single samples.
-    const {
-        error,
-        references,
-        subtractions,
-        workflows,
-        setError,
-        setReferences,
-        setSubtractions,
-        setWorkflows
-    } = useCreateAnalysis(mode);
 
     return (
         <Modal label="Quick Analyze" show={show} size="lg" onHide={onHide}>
@@ -108,41 +114,54 @@ export const QuickAnalyze = ({
                 )}
                 <QuickAnalyzeSelected>{samples.length} samples selected</QuickAnalyzeSelected>
             </ModalTabs>
-            <form onSubmit={handleSubmit}>
-                <ModalBody>
-                    {mode === "genome" && <HMMAlert />}
-                    <SelectedSamples samples={compatibleSamples} />
-                    <WorkflowSelector
-                        dataType={mode || "genome"}
-                        hasHmm={hasHmm}
-                        workflows={workflows}
-                        onSelect={setWorkflows}
-                    />
-                    {mode === "genome" && (
-                        <SubtractionSelector
-                            subtractions={subtractionOptions}
-                            value={subtractions}
-                            onChange={e => setSubtractions(e.target.value)}
-                        />
-                    )}
-                    <ReferenceSelector
-                        error={error}
-                        indexes={compatibleIndexes}
-                        selected={references}
-                        onChange={setReferences}
-                    />
-                </ModalBody>
-                <QuickAnalyzeFooter>
-                    <CreateAnalysisSummary
-                        indexCount={references.length}
-                        sampleCount={compatibleSamples.length}
-                        workflowCount={workflows.length}
-                    />
-                    <Button type="submit" color="blue" icon="play">
-                        Start
-                    </Button>
-                </QuickAnalyzeFooter>
-            </form>
+
+            <Formik onSubmit={handleSubmit} initialValues={initialValues} validationSchema={validationSchema}>
+                {({ errors, setFieldValue, touched, values }) => (
+                    <Form>
+                        <ModalBody>
+                            {mode === "genome" && <HMMAlert />}
+                            <SelectedSamples samples={compatibleSamples} />
+                            <Field
+                                as={WorkflowSelector}
+                                dataType={mode || "genome"}
+                                hasHmm={hasHmm}
+                                workflows={values.workflows}
+                                onSelect={workflows => setFieldValue("workflows", workflows)}
+                            />
+                            <QuickAnalyzeError>{touched.workflows && errors.workflows}</QuickAnalyzeError>
+                            {mode === "genome" && (
+                                <>
+                                    <Field
+                                        as={SubtractionSelector}
+                                        subtractions={subtractionOptions}
+                                        value={values.subtractions}
+                                        onChange={value => setFieldValue("subtractions", value)}
+                                    />
+
+                                    <QuickAnalyzeError>{touched.subtractions && errors.subtractions}</QuickAnalyzeError>
+                                </>
+                            )}
+                            <Field
+                                as={ReferenceSelector}
+                                indexes={compatibleIndexes}
+                                selected={values.references}
+                                onChange={value => setFieldValue("references", value)}
+                            />
+                            <QuickAnalyzeError>{touched.references && errors.references}</QuickAnalyzeError>{" "}
+                        </ModalBody>
+                        <QuickAnalyzeFooter>
+                            <CreateAnalysisSummary
+                                indexCount={values.references.length}
+                                sampleCount={compatibleSamples.length}
+                                workflowCount={values.workflows.length}
+                            />
+                            <Button type="submit" color="blue" icon="play">
+                                Start
+                            </Button>
+                        </QuickAnalyzeFooter>
+                    </Form>
+                )}
+            </Formik>
         </Modal>
     );
 };
