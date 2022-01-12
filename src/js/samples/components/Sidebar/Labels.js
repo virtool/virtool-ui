eb020e021138d68b7eb6876446748085e9cb33cd
reecehoffmann <reecehoffmann@outlook.com>
2022-01-12 10:44:46 -0800

    fix: disable sample creation sidebar dropdowns when no options available

--- src/js/samples/components/Sidebar/Labels.js
+++ src/js/samples/components/Sidebar/Labels.js
@@ -4,13 +4,26 @@ import { SidebarHeader, SideBarSection } from "../../../base";
 import { SmallSampleLabel } from "../Label";
 import { SampleSidebarList } from "./List";
 import { SampleSidebarSelector } from "./Selector";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { getFontSize, fontWeight, getColor } from "../../../app/theme";
 
 const SampleLabelInner = ({ name, color, description }) => (
-    <React.Fragment>
+    <>
         <SmallSampleLabel color={color} name={name} />
         <p>{description}</p>
-    </React.Fragment>
+    </>
 );
+
+const SampleLabelsFooter = styled.div`
+    display: flex;
+    color: ${props => getColor({ theme: props.theme, color: "greyDarkest" })};
+    a {
+        margin-left: 5px;
+        font-size: ${getFontSize("md")};
+        font-weight: ${fontWeight.thick};
+    }
+`;
 
 export const SampleLabels = ({ allLabels, sampleLabels, onUpdate }) => (
     <SideBarSection>
@@ -24,9 +37,15 @@ export const SampleLabels = ({ allLabels, sampleLabels, onUpdate }) => (
                 selectedItems={sampleLabels}
                 onUpdate={onUpdate}
                 selectionType="labels"
+                manageLink={"/samples/labels"}
             />
         </SidebarHeader>
         <SampleSidebarList items={allLabels.filter(item => sampleLabels.includes(item.id))} />
+        {Boolean(allLabels.length) || (
+            <SampleLabelsFooter>
+                No labels found. <Link to="/samples/labels">Create one</Link>.
+            </SampleLabelsFooter>
+        )}
     </SideBarSection>
 );
 
--- src/js/samples/components/Sidebar/Selector.js
+++ src/js/samples/components/Sidebar/Selector.js
@@ -5,15 +5,35 @@ import { BoxGroupSection, Icon, Input, SidebarHeaderButton } from "../../../base
 import { useFuse } from "../../../base/hooks";
 import { PopoverBody, usePopover } from "../../../base/Popover";
 import { SampleSidebarSelectorItem } from "./SelectorItem";
+import { Link } from "react-router-dom";
+import { getFontSize, fontWeight } from "../../../app/theme";
 
 const SampleSidebarSelectorInputContainer = styled(BoxGroupSection)`
     padding: 10px;
 `;
 
-export const SampleSidebarSelector = ({ render, sampleItems, selectedItems, sampleId, onUpdate, selectionType }) => {
+const StyledLinkButton = styled.div`
+    display: flex;
+
+    a {
+        margin-left: auto;
+        font-size: ${getFontSize("md")};
+        font-weight: ${fontWeight.thick};
+        padding: 10px 10px 10px 0px;
+    }
+`;
+
+export const SampleSidebarSelector = ({
+    render,
+    sampleItems,
+    selectedItems,
+    sampleId,
+    onUpdate,
+    selectionType,
+    manageLink
+}) => {
     const [results, term, setTerm] = useFuse(sampleItems, ["name"], [sampleId]);
     const [attributes, show, styles, setPopperElement, setReferenceElement, setShow] = usePopover();
-
     const handleToggle = useCallback(
         itemId => {
             onUpdate(xor(selectedItems, [itemId]));
@@ -32,15 +52,17 @@ export const SampleSidebarSelector = ({ render, sampleItems, selectedItems, samp
     ));
 
     return (
-        <React.Fragment>
-            <SidebarHeaderButton
-                aria-label={"select " + selectionType}
-                type="button"
-                ref={setReferenceElement}
-                onClick={setShow}
-            >
-                <Icon name="pen" />
-            </SidebarHeaderButton>
+        <>
+            {!sampleItems.length || (
+                <SidebarHeaderButton
+                    aria-label={`select ${selectionType}`}
+                    type="button"
+                    ref={setReferenceElement}
+                    onClick={setShow}
+                >
+                    <Icon name="pen" />
+                </SidebarHeaderButton>
+            )}
             {show && (
                 <PopoverBody ref={setPopperElement} show={show} style={styles.popper} {...attributes.popper}>
                     <SampleSidebarSelectorInputContainer>
@@ -53,8 +75,11 @@ export const SampleSidebarSelector = ({ render, sampleItems, selectedItems, samp
                         />
                     </SampleSidebarSelectorInputContainer>
                     {sampleItemComponents}
+                    <StyledLinkButton>
+                        <Link to={manageLink}> Manage</Link>
+                    </StyledLinkButton>
                 </PopoverBody>
             )}
-        </React.Fragment>
+        </>
     );
 };
