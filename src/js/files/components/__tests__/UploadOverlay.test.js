2a53fa52b5c7e7ce90877b3efa24f8c4a58c8567
elanjm12 <97321944+elanjm12@users.noreply.github.com>
2022-07-27 13:45:57 -0400

    fix: display message when finishing up uploads

--- src/js/files/components/__tests__/UploadOverlay.test.js
+++ src/js/files/components/__tests__/UploadOverlay.test.js
@@ -11,7 +11,7 @@ describe("<UploadOverlay />", () => {
                     fileType: "subtraction",
                     localId: "123abc",
                     name: "test_reads.fastq.gz",
-                    progress: 100,
+                    progress: 95,
                     size: 1024
                 },
                 {
@@ -38,7 +38,7 @@ describe("<UploadOverlay />", () => {
                 fileType: "subtraction",
                 localId: "123abc",
                 name: "test_reads.fastq.gz",
-                progress: 100,
+                progress: 99,
                 size: 871290
             }
         ];
@@ -49,6 +49,7 @@ describe("<UploadOverlay />", () => {
         expect(screen.getByText("871.3 KB")).toBeInTheDocument();
         expect(screen.getByText("1 hour remaining")).toBeInTheDocument();
         expect(screen.getByText("0 MB/s")).toBeInTheDocument();
+        expect(screen.queryByText("Finishing uploads")).not.toBeInTheDocument();
     });
 
     it("should render correctly when there are 3 uploads in progress", () => {
@@ -61,12 +62,36 @@ describe("<UploadOverlay />", () => {
         expect(screen.getByText("1.0 KB")).toBeInTheDocument();
         expect(screen.getByText("1 hour remaining")).toBeInTheDocument();
         expect(screen.getByText("0 MB/s")).toBeInTheDocument();
+        expect(screen.queryByText("Finishing uploads")).not.toBeInTheDocument();
     });
 
     it("should render null if no uploads in progress", () => {
         props.uploads = [];
         renderWithProviders(<UploadOverlay {...props} />);
         expect(screen.queryByText("Uploads")).not.toBeInTheDocument();
+        expect(screen.queryByText("NaN min remaining")).not.toBeInTheDocument();
+        expect(screen.queryByText("0.0 MB/s")).not.toBeInTheDocument();
+    });
+
+    it("should render correctly when all total uploads have reached 100% progress", () => {
+        props.uploads = [
+            {
+                fileType: "subtraction",
+                localId: "123abc",
+                name: "test_reads.fastq.gz",
+                progress: 100,
+                size: 1024
+            },
+            {
+                fileType: "reads",
+                localId: "456def",
+                name: "test_reads.fastq.gz2",
+                progress: 100,
+                size: 2025
+            }
+        ];
+        renderWithProviders(<UploadOverlay {...props} />);
+        expect(screen.getByText("Finishing uploads")).toBeInTheDocument();
         expect(screen.queryByText("NaN min remaining")).not.toBeInTheDocument();
         expect(screen.queryByText("0.0 MB/s")).not.toBeInTheDocument();
     });
