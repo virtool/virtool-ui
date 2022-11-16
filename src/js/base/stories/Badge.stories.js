68e80f735676c87e33fc0b21f70bd44d01dcd598
elanjm12 <97321944+elanjm12@users.noreply.github.com>
2022-11-16 15:34:02 -0800

    docs: add badge to storybook

---
+++ src/js/base/stories/Badge.stories.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Badge } from "../Badge";
+import styled from "styled-components";
+
+const StyledBadgeHeader = styled.div`
+    align-items: center;
+    display: flex;
+    strong {
+        margin-right: 5px;
+    }
+`;
+
+export default {
+    title: "base/Badge",
+    component: Badge,
+    parameters: {
+        docs: {
+            description: {
+                component: "Useful for highlighting the status or quantity of an item."
+            }
+        }
+    },
+    argTypes: {
+        color: {
+            options: ["greyDark", "blue", "orange", "purple", "red"],
+            control: { type: "radio" },
+            defaultValue: "greyDark"
+        },
+        children: {
+            type: "number",
+            defaultValue: 10
+        }
+    }
+};
+
+const Template = args => (
+    <StyledBadgeHeader>
+        <strong>Files Uploaded</strong>
+        <Badge {...args} />
+    </StyledBadgeHeader>
+);
+
+export const sampleBadge = Template.bind({});
