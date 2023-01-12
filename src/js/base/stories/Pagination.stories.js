376e01ed21bddcecf2ea5946a2a1d23d59d1d109
Reece Hoffmann <reecehoffmann@outlook.com>
2023-01-12 11:13:23 -0800

    fix: enable navigation in pagination storybook

--- src/js/base/stories/Pagination.stories.js
+++ src/js/base/stories/Pagination.stories.js
@@ -1,8 +1,13 @@
-import React, { useState } from "react";
+import React, { useMemo } from "react";
 import { Pagination } from "../Pagination";
+import { useArgs } from "@storybook/client-api";
+import { faker } from "@faker-js/faker";
+import { map } from "lodash-es";
+import { UserItem } from "../../users/components/Item";
+import { Box } from "../";
 
 export default {
-    title: "base/pagination",
+    title: "base/Pagination",
     component: Pagination,
     parameters: {
         docs: {
@@ -15,13 +20,35 @@ export default {
 };
 
 const Template = args => {
-    const [currentPage, setCurrentPage] = useState(1);
-    return <Pagination onLoadNextPage={page => setCurrentPage(page)} currentPage={currentPage} {...args} />;
+    const [_, updateArgs] = useArgs();
+    const items = useMemo(() => fakeUserListFactory(args.storedPage, 7), [args.storedPage]);
+    const onLoadNextPage = page => {
+        updateArgs({ currentPage: page, storedPage: page });
+    };
+
+    return (
+        <Box>
+            <Pagination onLoadNextPage={onLoadNextPage} renderRow={UserItem} items={items} {...args} />
+        </Box>
+    );
 };
 
-export const testPagination = Template.bind({});
+export const ExamplePagination = Template.bind({});
 
-testPagination.args = {
+ExamplePagination.args = {
     pageCount: 10,
-    currentPage: 1
+    currentPage: 1,
+    storedPage: 1
 };
+
+const fakeUserListFactory = (seed, numItems) => {
+    faker.seed(seed);
+    let userList = Array(numItems);
+    return map(userList, () => fakeUserFactory());
+};
+
+const fakeUserFactory = () => ({
+    id: faker.random.alphaNumeric(6),
+    handle: `${faker.name.firstName()}${faker.name.lastName()}`,
+    administrator: false
+});
