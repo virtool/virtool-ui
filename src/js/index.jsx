d3acce4b03b1d3f2e3fa38b7682460a514d69a17
igboyes <igboyes@gmail.com>
2023-04-25 14:01:55 -0700

    fix: restore sentry error reporting
    * Error reports were not being sent to Sentry.
    * Enable Redux error integration.
    * Enable profiling.

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,32 +1,36 @@
-import * as Sentry from "@sentry/browser";
+import * as Sentry from "@sentry/react";
+import { withProfiler } from "@sentry/react";
 import { createBrowserHistory } from "history";
-import "./nonce";
 import "normalize.css";
 import React from "react";
 import ReactDOM from "react-dom";
+import { setInitialState } from "./app/actions";
 import App from "./app/App";
 import { createAppStore } from "./app/reducer";
 import { Request } from "./app/request";
-import { setInitialState } from "./app/actions";
+import "./nonce";
 
-window.captureException = error => console.error(error);
-
-if (window.__sentry_dsn__ !== "SENTRY_DSN") {
+if (window.virtool.sentryDsn !== "SENTRY_DSN") {
     window.captureException = error => Sentry.captureException(error);
 
     Sentry.init({
-        dsn: window.__sentry_dsn__
+        dsn: window.virtool.sentryDsn,
+        integrations: [new Sentry.BrowserTracing()],
+        tracesSampleRate: 0.5
     });
+} else {
+    window.captureException = error => console.error(error);
 }
 
 const history = createBrowserHistory();
 
 window.b2c = { use: false };
-
 window.store = createAppStore(history);
 
 Request.get("/api").then(({ body }) => {
     window.store.dispatch(setInitialState(body));
 });
 
-ReactDOM.render(<App store={window.store} history={history} />, document.getElementById("app-container"));
+const AppWithProfiler = withProfiler(App);
+
+ReactDOM.render(<AppWithProfiler store={window.store} history={history} />, document.getElementById("app-container"));
