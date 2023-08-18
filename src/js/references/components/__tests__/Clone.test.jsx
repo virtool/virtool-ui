080304c60ae66fe2eda4936474097f02a969447a
Reece Hoffmann <reecehoffmann@outlook.com>
2023-08-18 15:44:54 -0700

    fix: prevent snapshots failures from changing dates

--- src/js/references/components/__tests__/Clone.test.jsx
+++ src/js/references/components/__tests__/Clone.test.jsx
@@ -16,7 +16,7 @@ describe("<CloneReference />", () => {
                 name: "foo_name",
                 otu_count: 5,
                 user: { id: "foo_user_id", handle: "foo_user_handle" },
-                created_at: "2019-02-10T17:11:00.000000Z",
+                created_at: new Date().setFullYear(new Date().getFullYear() - 1),
             },
         ],
         show: true,
@@ -31,7 +31,7 @@ describe("<CloneReference />", () => {
         expect(screen.getByText("5 OTUs")).toBeInTheDocument();
         expect(screen.getByText("foo_name")).toBeInTheDocument();
         expect(screen.getByText(/foo_user_handle.+created/)).toBeInTheDocument();
-        expect(screen.getByText("4 years ago")).toBeInTheDocument();
+        expect(screen.getByText("1 year ago")).toBeInTheDocument();
         expect(screen.getByRole("textbox")).toHaveValue("Clone of foo_name");
         expect(screen.getByRole("button", { name: "Clone" })).toBeInTheDocument();
     });
