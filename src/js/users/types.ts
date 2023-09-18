a5eed056a6fd743823e39e130a8f69b2e235dc18
Rastin Rashidi <rastinrashidi@gmail.com>
2023-09-18 13:42:02 -0700

    chore: cleanup user typing

--- src/js/users/types.ts
+++ src/js/users/types.ts
@@ -1,42 +1,68 @@
 import { AdministratorRoles } from "../administration/types";
 import { GroupMinimal, Permissions } from "../groups/types";
 
+/* Business to consumer provided user details */
 type UserB2C = {
+    /* The display name */
     display_name?: string;
+    /* The family name */
     family_name?: string;
+    /* The given name */
     given_name?: string;
+    /* The object ID */
     oid: string;
 };
 
-export type User = {
-    id: string;
+/* A user with the essential information */
+export type UserNested = {
+    /* Indicates if the user is an administrator */
     administrator: boolean;
+    /* The unique identifier */
+    id: string;
+    /* The user's handle or username */
     handle: string;
+};
+
+/* A Virtool user */
+export type User = UserNested & {
+    /* Indicates if user is active */
     active: boolean;
+    /* Their B2C specific information */
     b2c?: UserB2C;
+    /* Their display name */
     b2c_display_name?: string;
+    /* Their family name */
     b2c_family_name?: string;
+    /* Their given name */
     b2c_given_name?: string;
+    /* Their B2C object ID */
     b2c_oid?: string;
+    /* Whether the user will be forced to reset their password on next login */
     force_reset: boolean;
+    /* A list of their groups */
     groups: Array<GroupMinimal>;
+    /* The date of their last password change */
     last_password_change: Date;
+    /* Their permissions */
     permissions: Permissions;
+    /* Their primary group */
     primary_group: GroupMinimal;
+    /* Their administrator role defining what resources they can modify */
     administrator_role: AdministratorRoles;
 };
 
+/* User search results from the API */
 export type UserResponse = {
+    /* The page of users */
     items: Array<User>;
+    /* The number of users found */
     found_count: number;
+    /* The current page number */
     page: number;
+    /* The total number of pages */
     page_count: number;
+    /* The number of items per page */
     per_page: number;
+    /* The total number of users */
     total_count: number;
-};
-
-export type UserNested = {
-    administrator: boolean;
-    id: string;
-    handle: string;
 };
