72b897d391f4e4f78a8d8ea91d2ea45138540281
Rastin Rashidi <rastinrashidi@gmail.com>
2023-09-18 16:40:42 -0700

    chore: modernise the subtractions list view

--- src/js/subtraction/components/List.jsx
+++ src/js/subtraction/components/List.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { checkAdminRoleOrPermission } from "../../administration/utils";
 import { Badge, LegacyScrollList, LoadingPlaceholder, NoneFoundBox, ViewHeader, ViewHeaderTitle } from "../../base";
@@ -6,53 +6,57 @@ import { findSubtractions } from "../actions";
 import SubtractionItem from "./Item";
 import SubtractionToolbar from "./Toolbar";
 
-export class SubtractionList extends React.Component {
-    componentDidMount() {
-        if (!this.props.fetched) {
-            this.props.onLoadNextPage(this.props.term, 1);
+export const SubtractionList = ({ onLoadNextPage, documents, page, page_count, total_count, fetched, term }) => {
+    useEffect(() => {
+        if (!fetched) {
+            onLoadNextPage(term, 1);
         }
+    }, []);
+
+    const renderRow = index => <SubtractionItem key={index} index={index} />;
+    let subtractionComponents;
+
+    if (documents === null) {
+        return <LoadingPlaceholder />;
     }
 
-    renderRow = index => <SubtractionItem key={index} index={index} />;
-
-    render() {
-        let subtractionComponents;
-        if (this.props.documents === null) {
-            return <LoadingPlaceholder />;
-        }
-
-        if (this.props.documents.length) {
-            subtractionComponents = (
-                <LegacyScrollList
-                    documents={this.props.documents}
-                    onLoadNextPage={page => this.props.onLoadNextPage(this.props.term, page)}
-                    page={this.props.page}
-                    pageCount={this.props.page_count}
-                    renderRow={this.renderRow}
-                />
-            );
-        } else {
-            subtractionComponents = <NoneFoundBox noun="subtractions" />;
-        }
-
-        return (
-            <>
-                <ViewHeader title="Subtractions">
-                    <ViewHeaderTitle>
-                        Subtractions <Badge>{this.props.total_count}</Badge>
-                    </ViewHeaderTitle>
-                </ViewHeader>
-
-                <SubtractionToolbar />
-
-                {subtractionComponents}
-            </>
+    if (documents.length) {
+        subtractionComponents = (
+            <LegacyScrollList
+                documents={documents}
+                onLoadNextPage={page => onLoadNextPage(term, page)}
+                page={page}
+                pageCount={page_count}
+                renderRow={renderRow}
+            />
         );
+    } else {
+        subtractionComponents = <NoneFoundBox noun="subtractions" />;
     }
-}
+
+    return (
+        <>
+            <ViewHeader title="Subtractions">
+                <ViewHeaderTitle>
+                    Subtractions <Badge>{total_count}</Badge>
+                </ViewHeaderTitle>
+            </ViewHeader>
+
+            <SubtractionToolbar />
+
+            {subtractionComponents}
+        </>
+    );
+};
 
 const mapStateToProps = state => ({
     ...state.subtraction,
+    fetched: state.subtraction.fetched,
+    documents: state.subtraction.documents,
+    page: state.subtraction.page,
+    page_count: state.subtraction.page_count,
+    total_count: state.subtraction.total_count,
+    term: state.subtraction.term,
     canModify: checkAdminRoleOrPermission(state, "modify_subtraction"),
 });
 
