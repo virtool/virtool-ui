60a6425772306a36f033e0e6491f7a42b68cb834
Rastin Rashidi <rastinrashidi@gmail.com>
2023-09-18 16:56:27 -0700

    chore: add job models

--- src/js/jobs/types.ts
+++ src/js/jobs/types.ts
@@ -1,4 +1,5 @@
 import { UserNested } from "../users/types";
+import { SearchResult } from "../utils/types";
 
 export type JobState =
     | "complete"
@@ -26,6 +27,7 @@ export type JobMinimal = JobNested & {
     workflow: string;
 };
 
+/* Ping information of job */
 export type JobPing = {
     pinged_at: Date;
 };
@@ -42,26 +44,29 @@ export type JobStatus = {
     /* Indicates job error if occurred */
     error?: JobError | null;
     progress: number;
+    /* Stage description for job */
     stage?: string | null;
+    /* Current state of job */
     state: JobState;
+    /* Description of current step */
     step_description?: string | null;
+    /* Name of current step */
     step_name?: string | null;
     timestamp: Date;
 };
 
 export type Job = JobMinimal & {
+    /* Indicates whether job has been acquired */
     acquired: boolean;
+    /*  */
     args: { [key: string]: any };
+    /* Array containing status history of job */
     status: Array<JobStatus>;
     ping?: JobPing;
 };
 
-export type JobSearchResult = {
-    found_count: number;
-    page: number;
-    page_count: number;
-    per_page: number;
-    total_count: number;
+/* Job search results from the API */
+export type JobSearchResult = SearchResult & {
     /* Gives information about number of jobs in each state */
     counts: {
         [state in JobState]?: {
