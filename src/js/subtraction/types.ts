e511a07ad9a481c5c0be8ee46e49096193dafa49
Rastin Rashidi <rastinrashidi@gmail.com>
2023-09-19 14:13:41 -0700

    chore: fix subtraction type docstrings

--- src/js/subtraction/types.ts
+++ src/js/subtraction/types.ts
@@ -1,22 +1,16 @@
 import { JobMinimal } from "../jobs/types";
+
 import { SampleNested } from "../samples/types";
 import { UserNested } from "../users/types";
 import { SearchResult } from "../utils/types";
 
-<<<<<<< HEAD
 /* The measurements of individual nucleotides (percentage) */
-=======
-/* The composition of a Nucleotide */
->>>>>>> ui-64-add-subtraction-models
 export type NucleotideComposition = {
     a: number;
     c: number;
     g: number;
     t: number;
-<<<<<<< HEAD
     /* Unknown nucleotide */
-=======
->>>>>>> ui-64-add-subtraction-models
     n: number;
 };
 
@@ -25,26 +19,16 @@ export type SubtractionFile = {
     download_url: string;
     id: number;
     name: string;
-<<<<<<< HEAD
     /* The size of file (bytes) */
     size: number;
     /* The subtraction ID */
-=======
-    size: number;
->>>>>>> ui-64-add-subtraction-models
     subtraction: string;
     type: string;
 };
 
 /* A Subtraction with essential information  */
 export type SubtractionNested = {
-<<<<<<< HEAD
     id: string;
-=======
-    /* The unique identifier */
-    id: string;
-    /* The subtraction's name */
->>>>>>> ui-64-add-subtraction-models
     name: string;
 };
 
@@ -54,7 +38,6 @@ export type SubtractionUpload = {
     name: string;
 };
 
-<<<<<<< HEAD
 /* Minimal Subtraction used for websocket messages and resource listings */
 export type SubtractionMinimal = SubtractionNested & {
     count?: number;
@@ -70,23 +53,6 @@ export type SubtractionMinimal = SubtractionNested & {
 };
 
 /* A complete Subtraction */
-=======
-/* Minimal Subtraction model used for websocket messages and resource listings */
-export type SubtractionMinimal = SubtractionNested & {
-    count?: number;
-    created_at: Date;
-    /* Name of associated subtraction file */
-    file: SubtractionUpload;
-    /* Information related to job of subtraction upload */
-    job?: JobMinimal;
-    nickname: string;
-    ready: boolean;
-    /* The associated user */
-    user?: UserNested;
-};
-
-/* Complete Subtraction model */
->>>>>>> ui-64-add-subtraction-models
 export type Subtraction = SubtractionMinimal & {
     /* Data files available to workflows */
     files: Array<SubtractionFile>;
