1a36aeb7ad3b2ea9015138041168752e0c85ef36
Rastin Rashidi <rastinrashidi@gmail.com>
2023-09-20 10:04:10 -0700

    fix: docstrings

--- src/js/jobs/types.ts
+++ src/js/jobs/types.ts
@@ -13,10 +13,12 @@ export type JobState =
 
 export type IconColor = "blue" | "green" | "grey" | "red" | "orange" | "purple";
 
+/* A Job with essential information */
 export type JobNested = {
     id: string;
 };
 
+/* Minimal Job used for websocket messages and resource listings */
 export type JobMinimal = JobNested & {
     archived: boolean;
     created_at: Date;
@@ -27,38 +29,34 @@ export type JobMinimal = JobNested & {
     workflow: string;
 };
 
-/* Ping information of job */
+/* Provides information on when a Job was pinged */
 export type JobPing = {
     pinged_at: Date;
 };
 
+/* Provides Job error information */
 export type JobError = {
-    /* Details of error */
     details: Array<string>;
-    /* Traceback of error */
     traceback: Array<string>;
     type: string;
 };
 
+/* Provides Job Status information */
 export type JobStatus = {
-    /* Indicates job error if occurred */
     error?: JobError | null;
     progress: number;
     /* Stage description for job */
     stage?: string | null;
-    /* Current state of job */
     state: JobState;
-    /* Description of current step */
     step_description?: string | null;
-    /* Name of current step */
     step_name?: string | null;
     timestamp: Date;
 };
 
+/* A complete Job */
 export type Job = JobMinimal & {
-    /* Indicates whether job has been acquired */
     acquired: boolean;
-    /*  */
+    /* Provides information on subtraction and the related files */
     args: { [key: string]: any };
     /* Array containing status history of job */
     status: Array<JobStatus>;
