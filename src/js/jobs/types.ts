8da6760d9872d457669f480fd489a2e5c927c987
Rastin Rashidi <rastinrashidi@gmail.com>
2023-09-25 09:16:23 -0700

    chore: add job models

--- src/js/jobs/types.ts
+++ src/js/jobs/types.ts
@@ -1,4 +1,5 @@
 import { UserNested } from "../users/types";
+import { SearchResult } from "../utils/types";
 
 export type JobState =
     | "complete"
@@ -12,10 +13,12 @@ export type JobState =
 
 export type IconColor = "blue" | "green" | "grey" | "red" | "orange" | "purple";
 
+/* A Job with essential information */
 export type JobNested = {
     id: string;
 };
 
+/* Minimal Job used for websocket messages and resource listings */
 export type JobMinimal = JobNested & {
     archived: boolean;
     created_at: Date;
@@ -24,4 +27,50 @@ export type JobMinimal = JobNested & {
     state: JobState;
     user: UserNested;
     workflow: string;
+};
+
+/* Provides information on when a Job was pinged */
+export type JobPing = {
+    pinged_at: Date;
+};
+
+/* Provides Job error information */
+export type JobError = {
+    details: Array<string>;
+    traceback: Array<string>;
+    type: string;
+};
+
+/* Provides Job Status information */
+export type JobStatus = {
+    error?: JobError | null;
+    progress: number;
+    /* Stage description for job */
+    stage?: string | null;
+    state: JobState;
+    step_description?: string | null;
+    step_name?: string | null;
+    timestamp: Date;
+};
+
+/* A complete Job */
+export type Job = JobMinimal & {
+    acquired: boolean;
+    /* Provides information on subtraction and the related files */
+    args: { [key: string]: any };
+    /* Array containing status history of job */
+    status: Array<JobStatus>;
+    ping?: JobPing;
+};
+
+/* Job search results from the API */
+export type JobSearchResult = SearchResult & {
+    /* Gives information about number of jobs in each state */
+    counts: {
+        [state in JobState]?: {
+            [key: string]: number | null;
+        };
+    };
+    /* Gives information about each job */
+    documents: Array<JobMinimal>;
 };
