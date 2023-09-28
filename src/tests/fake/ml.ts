4695505d80df70fa74f4a5f2746258c3c1ad50e9
Reece Hoffmann <reecehoffmann@outlook.com>
2023-09-28 11:09:01 -0700

    style: remove unused ML model faking overrides

--- src/tests/fake/ml.ts
+++ src/tests/fake/ml.ts
@@ -3,25 +3,15 @@ import { merge } from "lodash";
 import nock from "nock";
 import { MLModel, MLModelMinimal, MLModelRelease } from "../../js/ml/types";
 
-type CreateFakeMLModelReleaseOverrides = {
-    id?: number;
-    created_at?: string;
-    download_url?: string;
-    github_url?: string;
-    name?: string;
-    published_at?: string;
-    ready?: boolean;
-    size?: number;
-};
-
 /**
  * Create a fake ML model release object
  *
  * @param overrides - optional properties for creating a fake ML models with specific values
  */
-export function createFakeMLModelRelease(overrides?: CreateFakeMLModelReleaseOverrides): MLModelRelease {
-    const id = overrides?.id ?? faker.datatype.number(100);
-    const defaultRelease = {
+export function createFakeMLModelRelease(): MLModelRelease {
+    const id = faker.datatype.number(100);
+
+    return {
         id,
         created_at: faker.date.past().toISOString(),
         download_url: `api/ml/${id}/download`,
@@ -31,17 +21,10 @@ export function createFakeMLModelRelease(overrides?: CreateFakeMLModelReleaseOve
         ready: true,
         size: faker.datatype.number(100000),
     };
-
-    return merge(defaultRelease, overrides);
 }
 
-type CreateFakeMLModelMinimalOverrides = {
-    id?: number;
+type CreateFakeMLModelOverrides = {
     created_at?: string;
-    description?: string;
-    latest_release?: MLModelRelease | null;
-    name?: string;
-    release_count?: number;
 };
 
 /**
@@ -49,22 +32,16 @@ type CreateFakeMLModelMinimalOverrides = {
  *
  * @param overrides - optional properties for creating a fake ML models with specific values
  */
-export function createFakeMLModelMinimal(overrides?: CreateFakeMLModelMinimalOverrides): MLModelMinimal {
-    const defaultMinimal = {
+export function createFakeMLModelMinimal(overrides?: CreateFakeMLModelOverrides): MLModelMinimal {
+    return {
         id: faker.datatype.number(100),
-        created_at: faker.date.past().toISOString(),
+        created_at: overrides.created_at ?? faker.date.past().toISOString(),
         description: faker.lorem.sentence(),
-        latest_release: createFakeMLModelRelease(overrides?.latest_release),
+        latest_release: createFakeMLModelRelease(),
         name: `model-${faker.lorem.word()}`,
         release_count: faker.datatype.number(10),
     };
-
-    return merge(defaultMinimal, overrides);
 }
-
-type CreateFakeMLModelOverrides = CreateFakeMLModelMinimalOverrides & {
-    releases?: MLModelRelease[];
-};
 
 /**
  * Create a fake complete ML model
@@ -72,7 +49,7 @@ type CreateFakeMLModelOverrides = CreateFakeMLModelMinimalOverrides & {
  * @param overrides - optional properties for creating a fake ML models with specific values
  */
 export function createFakeMLModel(overrides?: CreateFakeMLModelOverrides): MLModel {
-    const releases = overrides?.releases ?? [createFakeMLModelRelease()];
+    const releases = [createFakeMLModelRelease()];
 
     const defaultModel = {
         ...createFakeMLModelMinimal(overrides),
@@ -90,6 +67,8 @@ export function createFakeMLModel(overrides?: CreateFakeMLModelOverrides): MLMod
  * @param last_synced_at - The date the models were last synced with virtool.ca
  * @returns The nock scope for the mocked API call
  */
-export function mockApiGetModels(MLModels: MLModelMinimal[], last_synced_at?: string) {
-    return nock("http://localhost").get("/api/ml").reply(200, { items: MLModels, last_synced_at });
+export function mockApiGetModels(MLModels: MLModelMinimal[]) {
+    return nock("http://localhost")
+        .get("/api/ml")
+        .reply(200, { items: MLModels, last_synced_at: faker.date.recent().toISOString() });
 }
