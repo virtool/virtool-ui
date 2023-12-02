2c14c44e39d07fa65b3d5fc89fb3ae4a16b8601b
Rastin Rashidi <rastinrashidi@gmail.com>
2023-12-01 16:09:38 -0800

    chore: update user administration view implementation

---
+++ src/js/users/components/UserGroup.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+import { Checkbox, SelectBoxGroupSection } from "../../base";
+
+const StyledUserGroup = styled(SelectBoxGroupSection)`
+    text-transform: capitalize;
+    user-select: none;
+`;
+
+type UserGroupTypes = {
+    /** The group unique id */
+    id: string | number;
+    /** The group name */
+    name: string;
+    /** Whether the group is selected */
+    toggled: boolean;
+    /** A callback function to handle the selection of a group */
+    onClick: (id: string | number) => void;
+};
+
+/**
+ * A condensed user group for use in a list of user groups
+ */
+export function UserGroup({ id, name, toggled, onClick }: UserGroupTypes) {
+    return (
+        <StyledUserGroup active={toggled} onClick={() => onClick(id)}>
+            <Checkbox checked={toggled} label={name} />
+        </StyledUserGroup>
+    );
+}
--- src/js/users/types.ts
+++ src/js/users/types.ts
@@ -2,58 +2,58 @@ import { AdministratorRoles } from "../administration/types";
 import { GroupMinimal, Permissions } from "../groups/types";
 import { SearchResult } from "../utils/types";
 
-/* Business to consumer provided user details */
+/** Business to consumer provided user details */
 type UserB2C = {
-    /* The display name */
+    /** The display name */
     display_name?: string;
-    /* The family name */
+    /** The family name */
     family_name?: string;
-    /* The given name */
+    /** The given name */
     given_name?: string;
-    /* The object ID */
+    /** The object ID */
     oid: string;
 };
 
-/* A user with the essential information */
+/** A user with the essential information */
 export type UserNested = {
-    /* Indicates if the user is an administrator */
+    /** Indicates if the user is an administrator */
     administrator: boolean;
-    /* The unique identifier */
+    /** The unique identifier */
     id: string;
-    /* The user's handle or username */
+    /** The user's handle or username */
     handle: string;
 };
 
-/* A Virtool user */
+/** A Virtool user */
 export type User = UserNested & {
-    /* Indicates if user is active */
+    /** Indicates if user is active */
     active: boolean;
-    /* Their B2C specific information */
+    /** Their B2C specific information */
     b2c?: UserB2C;
-    /* Their display name */
+    /** Their display name */
     b2c_display_name?: string;
-    /* Their family name */
+    /** Their family name */
     b2c_family_name?: string;
-    /* Their given name */
+    /** Their given name */
     b2c_given_name?: string;
-    /* Their B2C object ID */
+    /** Their B2C object ID */
     b2c_oid?: string;
-    /* Whether the user will be forced to reset their password on next login */
+    /** Whether the user will be forced to reset their password on next login */
     force_reset: boolean;
-    /* A list of their groups */
+    /** A list of their groups */
     groups: Array<GroupMinimal>;
-    /* The date of their last password change */
-    last_password_change: Date;
-    /* Their permissions */
+    /** The date of their last password change */
+    last_password_change: string;
+    /** Their permissions */
     permissions: Permissions;
-    /* Their primary group */
+    /** Their primary group */
     primary_group: GroupMinimal;
-    /* Their administrator role defining what resources they can modify */
+    /** Their administrator role defining what resources they can modify */
     administrator_role: AdministratorRoles;
 };
 
-/* User search results from the API */
+/** User search results from the API */
 export type UserResponse = SearchResult & {
-    /* The page of users */
+    /** The page of users */
     items: Array<User>;
 };
