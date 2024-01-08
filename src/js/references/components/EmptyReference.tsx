e722e15ef63e3e8fb9dfd38a20483594043b6d8c
Rastin Rashidi <rastinrashidi@gmail.com>
2024-01-08 13:30:55 -0800

    refactor: convert to useForm, change datatypeselection component to ts

--- src/js/references/components/EmptyReference.tsx
+++ src/js/references/components/EmptyReference.tsx
@@ -1,5 +1,5 @@
-import { Form, Formik, FormikErrors, FormikTouched } from "formik";
 import React from "react";
+import { useForm } from "react-hook-form";
 import * as Yup from "yup";
 import { Alert, Button } from "../../base";
 import { useCreateReference } from "../querys";
@@ -35,37 +35,27 @@ type formValues = {
 export default function EmptyReference() {
     const mutation = useCreateReference();
 
-    function handleSubmit({ name, description, dataType, organism }) {
+    function onSubmit({ name, description, dataType, organism }) {
         mutation.mutate({ name, description, dataType, organism });
     }
 
+    const {
+        formState: { errors },
+        handleSubmit,
+        control,
+        register,
+    } = useForm({ defaultValues: { name: "", description: "", dataType: "genome", organism: "" } });
+
     return (
-        <Formik initialValues={getInitialState()} onSubmit={handleSubmit} validationSchema={validationSchema}>
-            {({
-                errors,
-                touched,
-                setFieldValue,
-                values,
-            }: {
-                errors: FormikErrors<formValues>;
-                touched: FormikTouched<formValues>;
-                setFieldValue: (field: string, value: string) => void;
-                values: formValues;
-            }) => (
-                <Form>
-                    <Alert>
-                        <strong>Create an empty reference.</strong>
-                    </Alert>
-                    <ReferenceForm errors={errors} touched={touched} mode={"empty"} />
-                    <DataTypeSelection
-                        onSelect={datatype => setFieldValue("dataType", datatype)}
-                        dataType={values.dataType}
-                    />
-                    <Button type="submit" icon="save" color="blue">
-                        Save
-                    </Button>
-                </Form>
-            )}
-        </Formik>
+        <form onSubmit={handleSubmit(values => mutation.mutate({ ...values }))}>
+            <Alert>
+                <strong>Create an empty reference.</strong>
+            </Alert>
+            <ReferenceForm errors={errors} mode={"empty"} register={register} />
+            <DataTypeSelection onSelect={datatype => setFieldValue("dataType", datatype)} dataType={values.dataType} />
+            <Button type="submit" icon="save" color="blue">
+                Save
+            </Button>
+        </form>
     );
 }
--- src/js/references/components/ReferenceForm.tsx
+++ src/js/references/components/ReferenceForm.tsx
@@ -1,39 +1,37 @@
-import { Field } from "formik";
 import React from "react";
 import styled from "styled-components";
-import { Input, InputError, InputGroup, InputLabel, TextArea } from "../../base";
+import { InputError, InputGroup, InputLabel, InputSimple } from "../../base";
 
 const StyledInputGroup = styled(InputGroup)`
     padding-bottom: 0px;
 `;
 
-export const ReferenceForm = ({ errors, touched, mode }) => {
+export function ReferenceForm({ errors, mode, register }) {
     let organismComponent;
 
     if (mode === "empty" || mode === "edit") {
         organismComponent = (
             <InputGroup>
                 <InputLabel htmlFor="organism">Organism</InputLabel>
-                <Field as={Input} name="organism" id="organism" />
+                <InputSimple as="textarea" id="organism" {...register("organism")} />
             </InputGroup>
         );
     }
-    const nameError = touched.name && errors.name;
 
     return (
         <>
             <StyledInputGroup>
                 <InputLabel htmlFor="name">Name</InputLabel>
-                <Field name="name" id="name" as={Input} error={nameError} />
-                <InputError>{nameError}</InputError>
+                <InputSimple id="name" {...register("name", { required: "Required Field." })} />
+                <InputError>{errors.name?.message}</InputError>
             </StyledInputGroup>
 
             {organismComponent}
 
             <InputGroup>
                 <InputLabel htmlFor="description">Description</InputLabel>
-                <Field as={TextArea} name="description" id="description" />
+                <InputSimple as="textarea" id="description" {...register("description")} />
             </InputGroup>
         </>
     );
-};
+}
