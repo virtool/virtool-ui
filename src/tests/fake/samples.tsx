63e7d3070fa77c39c060a02b7981eef09d606ff1
Rastin Rashidi <rastinrashidi@gmail.com>
2024-01-24 14:18:07 -0800

    test: fix mocking of sample creation api

--- src/tests/fake/samples.tsx
+++ src/tests/fake/samples.tsx
@@ -2,14 +2,18 @@ import { faker } from "@faker-js/faker";
 import { assign } from "lodash";
 import nock from "nock";
 import { LabelNested } from "../../js/labels/types";
-import { LibraryType, SampleMinimal, WorkflowState } from "../../js/samples/types";
+import { LibraryType, Sample, SampleMinimal, WorkflowState } from "../../js/samples/types";
+import { SubtractionNested } from "../../js/subtraction/types";
 import { createFakeLabelNested } from "./labels";
+import { createFakeSubtractionNested } from "./subtractions";
 import { createFakeUserNested } from "./user";
 
 export type CreateFakeSampleMinimal = {
+    name?: string;
     labels?: LabelNested[] | number[];
     host?: string;
     isolate?: string;
+    library_type?: LibraryType;
 };
 
 /**
@@ -37,6 +41,38 @@ export function createFakeSampleMinimal(overrides?: CreateFakeSampleMinimal): Sa
     return assign(defaultSampleMinimal, overrides);
 }
 
+type CreateFakeSample = CreateFakeSampleMinimal & {
+    group?: number | string | null;
+    paired?: boolean;
+    locale?: string;
+    subtractions?: Array<SubtractionNested>;
+};
+
+/**
+ * Create a fake sample object
+ */
+export function createFakeSample(overrides?: CreateFakeSample): Sample {
+    const defaultSample = {
+        ...createFakeSampleMinimal(),
+        all_read: faker.datatype.boolean(),
+        all_write: faker.datatype.boolean(),
+        artifacts: [],
+        format: "fastq",
+        group: null,
+        group_read: faker.datatype.boolean(),
+        group_write: faker.datatype.boolean(),
+        hold: faker.datatype.boolean(),
+        is_legacy: faker.datatype.boolean(),
+        locale: faker.random.word(),
+        paired: faker.datatype.boolean(),
+        quality: null,
+        reads: [],
+        subtractions: [createFakeSubtractionNested()],
+    };
+
+    return assign(defaultSample, overrides);
+}
+
 /**
  * Creates a mocked API call for getting a paginated list of samples
  *
@@ -54,24 +90,43 @@ export function mockApiGetSamples(samples: SampleMinimal[]) {
     });
 }
 
-export type CreateSampleType = {
-    name: string;
-    isolate: string;
-    host: string;
-    locale: string;
-    library_type: string;
-    files: string[];
-    labels: number[];
-    subtractions: string[];
-    group: string | null;
-};
-
 /**
  * Creates a mocked API call for creating a sample
  *
- * @param createSample - The data needed to create a sample
+ * @param name - The name of the sample
+ * @param isolate - The sample isolate
+ * @param host - The sample host
+ * @param locale - The locale of the sample
+ * @param library_type - The library type of the sample
+ * @param files - The sample files used to create the sample
+ * @param labels - The labels associated with the sample
+ * @param subtractions - The subtractions associated with the sample
+ * @param group - The group associated with the sample
  * @returns The nock scope for the mocked API call
  */
-export function mockApiCreateSample(createSample: CreateSampleType) {
-    return nock("http://localhost").post("/api/samples", createSample).reply(201, createSample);
+export function mockApiCreateSample(
+    name: string,
+    isolate: string,
+    host: string,
+    locale: string,
+    library_type: LibraryType,
+    files: string[],
+    labels: number[],
+    subtractions: string[],
+    group: string | null,
+) {
+    const sample = createFakeSample({
+        name,
+        isolate,
+        host,
+        locale,
+        library_type,
+        labels,
+        subtractions: [createFakeSubtractionNested({ id: subtractions[0] })],
+        group,
+    });
+
+    return nock("http://localhost")
+        .post("/api/samples", { name, isolate, host, locale, library_type, files, labels, subtractions, group })
+        .reply(201, sample);
 }
