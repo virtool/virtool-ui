5cdadbee0e713c48eab9b9ec9761d9d597b838ce
Rastin Rashidi <rastinrashidi@gmail.com>
2024-03-05 11:54:34 -0800

    test: update test

--- src/js/analyses/components/__tests__/AnalysisItemRightIcon.test.tsx
+++ src/js/analyses/components/__tests__/AnalysisItemRightIcon.test.tsx
@@ -3,15 +3,13 @@ import userEvent from "@testing-library/user-event";
 import React from "react";
 import { describe, expect, it, vi } from "vitest";
 import { renderWithProviders } from "../../../../tests/setupTests";
-import { AnalysisItemRightIcon } from "../RightIcon";
+import { AnalysisItemRightIcon } from "../AnalysisItemRightIcon";
 
-describe("<RightIcon />", () => {
+describe("<AnalysisItemRightIcon />", () => {
     it("should render remove icon when [ready=true] and [canModify=true]", async () => {
         const onRemove = vi.fn();
 
-        const { rerender } = renderWithProviders(
-            <AnalysisItemRightIcon canModify={true} ready={true} onRemove={onRemove} />,
-        );
+        const { rerender } = renderWithProviders(<AnalysisItemRightIcon canModify={true} onRemove={onRemove} />);
 
         const button = screen.getByRole("button");
 
@@ -22,14 +20,9 @@ describe("<RightIcon />", () => {
         // Should call onRemove when clicked.
         expect(onRemove).toHaveBeenCalled();
 
-        rerender(<AnalysisItemRightIcon canModify={false} ready={true} onRemove={onRemove} />);
+        rerender(<AnalysisItemRightIcon canModify={false} onRemove={onRemove} />);
 
         // Should not be shown if the user can't modify the analysis.
         expect(screen.queryByRole("button")).not.toBeInTheDocument();
-
-        rerender(<AnalysisItemRightIcon canModify={true} ready={false} onRemove={onRemove} />);
-
-        expect(screen.queryByRole("button")).not.toBeInTheDocument();
-        expect(screen.getByLabelText("loading")).toBeInTheDocument();
     });
 });
