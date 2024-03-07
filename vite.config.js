8fdcd708d35e62c8cd28ea89ad8cdc4666293389
Reece Hoffmann <reecehoffmann@outlook.com>
2024-03-07 14:09:07 -0800

    chore: add import path aliases for all src directories

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,47 @@
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 import { createHtmlPlugin } from "vite-plugin-html";
 
 export default defineConfig({
-  root: "src",
-  build: {
-    outDir: "../dist",
-    sourcemap: true,
-  },
-  plugins: [
-    createHtmlPlugin({}),
-    react({
-      include: "**/*.{jsx,tsx}",
-    }),
-  ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src/js"),
+            "@account": path.resolve(__dirname, "src/js/account"),
+            "@administration": path.resolve(__dirname, "src/js/administration"),
+            "@app": path.resolve(__dirname, "src/js/app"),
+            "@base": path.resolve(__dirname, "src/js/base"),
+            "@files": path.resolve(__dirname, "src/js/files"),
+            "@forms": path.resolve(__dirname, "src/js/forms"),
+            "@groups": path.resolve(__dirname, "src/js/groups"),
+            "@hmms": path.resolve(__dirname, "src/js/hmms"),
+            "@indexes": path.resolve(__dirname, "src/js/indexes"),
+            "@jobs": path.resolve(__dirname, "src/js/jobs"),
+            "@labels": path.resolve(__dirname, "src/js/labels"),
+            "@message": path.resolve(__dirname, "src/js/message"),
+            "@ml": path.resolve(__dirname, "src/js/ml"),
+            "@nav": path.resolve(__dirname, "src/js/nav"),
+            "@otus": path.resolve(__dirname, "src/js/otus"),
+            "@quality": path.resolve(__dirname, "src/js/quality"),
+            "@references": path.resolve(__dirname, "src/js/references"),
+            "@samples": path.resolve(__dirname, "src/js/samples"),
+            "@sequences": path.resolve(__dirname, "src/js/sequences"),
+            "@subtraction": path.resolve(__dirname, "src/js/subtraction"),
+            "@types": path.resolve(__dirname, "src/js/types"),
+            "@users": path.resolve(__dirname, "src/js/users"),
+            "@utils": path.resolve(__dirname, "src/js/utils"),
+            "@wall": path.resolve(__dirname, "src/js/wall"),
+        },
+    },
+    root: "src",
+    build: {
+        outDir: "../dist",
+        sourcemap: true,
+    },
+    plugins: [
+        createHtmlPlugin({}),
+        react({
+            include: "**/*.{jsx,tsx}",
+        }),
+    ],
 });
