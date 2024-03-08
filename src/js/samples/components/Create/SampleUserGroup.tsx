83df597187a506d1af0641519fc6a6220f6a959f
Rastin Rashidi <rastinrashidi@gmail.com>
2024-03-08 10:24:30 -0800

    refactor: add typing to sample rights group selector

--- src/js/samples/components/Create/SampleUserGroup.tsx
+++ src/js/samples/components/Create/SampleUserGroup.tsx
@@ -2,12 +2,23 @@ import { map } from "lodash-es";
 import React from "react";
 import styled from "styled-components";
 import { InputGroup, InputLabel, InputSelect } from "../../../base";
+import { GroupMinimal } from "../../../groups/types";
 
 const SampleUserGroupItem = styled.option`
     text-transform: capitalize;
 `;
 
-export const SampleUserGroup = ({ selected, groups, onChange }) => {
+type SampleUserGroupProps = {
+    selected: string;
+    groups: GroupMinimal[];
+    /** A callback function to handle the user group change */
+    onChange: () => void;
+};
+
+/**
+ * A dropdown showing the user groups and its options
+ */
+export function SampleUserGroup({ selected, groups, onChange }: SampleUserGroupProps) {
     const groupComponents = map(groups, group => (
         <SampleUserGroupItem key={group.id} value={group.id}>
             {group.name}
@@ -16,8 +27,8 @@ export const SampleUserGroup = ({ selected, groups, onChange }) => {
 
     return (
         <InputGroup>
-            <InputLabel>User Group</InputLabel>
-            <InputSelect value={selected} onChange={onChange}>
+            <InputLabel htmlFor="userGroups">User Group</InputLabel>
+            <InputSelect id="userGroups" value={selected} onChange={onChange}>
                 <option key="none" value="none">
                     None
                 </option>
@@ -25,4 +36,4 @@ export const SampleUserGroup = ({ selected, groups, onChange }) => {
             </InputSelect>
         </InputGroup>
     );
-};
+}
---
+++ src/js/samples/components/Create/__tests__/SampleUserGroup.test.tsx
@@ -0,0 +1,31 @@
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import React from "react";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import { renderWithProviders } from "../../../../../tests/setupTests";
+import { SampleUserGroup } from "../SampleUserGroup";
+
+describe("SampleUserGroup", () => {
+    let props;
+    beforeEach(() => {
+        props = {
+            groups: [{ name: "bar", id: "bar_id" }],
+            onChange: vi.fn(),
+            selected: "",
+        };
+    });
+
+    it("should render", () => {
+        renderWithProviders(<SampleUserGroup {...props} />);
+
+        expect(screen.getByLabelText("User Group")).toBeInTheDocument();
+        expect(screen.getByText("None")).toBeInTheDocument();
+        expect(screen.getByText("bar")).toBeInTheDocument();
+    });
+    it("should call onChange input is changed", async () => {
+        renderWithProviders(<SampleUserGroup {...props} />);
+
+        await userEvent.selectOptions(screen.getByLabelText("User Group"), "bar");
+        expect(props.onChange).toHaveBeenCalled();
+    });
+});
