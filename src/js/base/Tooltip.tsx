fdf28e8ab078655aee6cbaf048c0c6615b1ece0e
Rastin Rashidi <rastinrashidi@gmail.com>
2024-03-20 16:33:59 -0700

    feat: use radix tooltip & uninstall react-tippy

--- src/js/base/Tooltip.tsx
+++ src/js/base/Tooltip.tsx
@@ -1,18 +1,80 @@
+import * as TooltipPrimitive from "@radix-ui/react-tooltip";
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-import { Tooltip as TippyTooltip, TooltipProps as TippyTooltipProps } from "react-tippy";
-import "react-tippy/dist/tippy.css";
-
-interface StyledTooltipProps extends TippyTooltipProps {
-    children: React.ReactNode;
-}
-
-const StyledTooltip = styled(TippyTooltip)<StyledTooltipProps>`
-    display: inline-flex !important;
+const slideUpAndFade = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(2px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
 `;
 
-StyledTooltip.displayName = "StyledTooltip";
+const slideDownAndFade = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(-2px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+const slideRightAndFade = keyframes`
+    from {
+    opacity: 0;
+    transform: translateX(-2px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+const slideLeftAndFade = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(2px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+const TooltipContent = styled(TooltipPrimitive.Content)`
+    border-radius: 4px;
+    padding: 10px 15px;
+    font-size: 15px;
+    line-height: 1;
+    background-color: rgba(0, 0, 0, 0.7);
+    color: white;
+    box-shadow: hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px;
+    user-select: none;
+    animation-duration: 400ms;
+    animation-timing-function: cubic-bezier(0.16, 1, 0.3, 1);
+    will-change: transform, opacity;
+
+    &[data-state="delayed-open"][data-side="top"] {
+        animation-name: ${slideDownAndFade};
+    }
+
+    &[data-state="delayed-open"][data-side="right"] {
+        animation-name: ${slideLeftAndFade};
+    }
+
+    &[data-state="delayed-open"][data-side="bottom"] {
+        animation-name: ${slideUpAndFade};
+    }
+
+    &[data-state="delayed-open"][data-side="left"] {
+        animation-name: ${slideRightAndFade};
+    }
+`;
 
 type TooltipProps = {
     children: React.ReactNode;
@@ -22,10 +84,16 @@ type TooltipProps = {
 
 export function Tooltip({ tip, position = "top", children }: TooltipProps) {
     return (
-        <StyledTooltip size="regular" title={tip} position={position} arrow>
-            {children}
-        </StyledTooltip>
+        <TooltipPrimitive.Provider>
+            <TooltipPrimitive.Root>
+                <TooltipPrimitive.Trigger asChild>{children}</TooltipPrimitive.Trigger>
+                <TooltipPrimitive.Portal>
+                    <TooltipContent side={position} sideOffset={5}>
+                        {tip}
+                        <TooltipPrimitive.Arrow />
+                    </TooltipContent>
+                </TooltipPrimitive.Portal>
+            </TooltipPrimitive.Root>
+        </TooltipPrimitive.Provider>
     );
 }
-
-Tooltip.displayName = "Tooltip";
