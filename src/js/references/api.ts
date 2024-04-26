8fc1320565af3617e3db35be891b8c7252e90a07
Rastin Rashidi <rastinrashidi@gmail.com>
2024-04-26 12:05:38 -0700

    chore: remove unused api

--- src/js/references/api.ts
+++ src/js/references/api.ts
@@ -1,4 +1,4 @@
-import { Request } from "../app/request";
+import { Request } from "@app/request";
 import {
     Reference,
     ReferenceDataType,
@@ -8,33 +8,12 @@ import {
     ReferenceUser,
 } from "./types";
 
-export function find({ term, page }) {
-    return Request.get("/refs").query({ find: term, page });
-}
-
 export function get({ refId }) {
     return Request.get(`/refs/${refId}`);
 }
 
-export function create({ name, description, dataType, organism }) {
-    return Request.post("/refs").send({
-        name,
-        description,
-        data_type: dataType,
-        organism,
-    });
-}
-
 export function edit({ refId, update }) {
     return Request.patch(`/refs/${refId}`).send(update);
-}
-
-export function importReference({ name, description, fileId }) {
-    return Request.post("/refs").send({
-        name,
-        description,
-        import_from: fileId,
-    });
 }
 
 /**
@@ -57,34 +36,6 @@ export function cloneReference({ name, description, refId }): Promise<ReferenceM
 
 export function remoteReference({ remote_from }) {
     return Request.post("/refs").send({ remote_from });
-}
-
-export function remove({ refId }) {
-    return Request.delete(`/refs/${refId}`);
-}
-
-export function addUser({ refId, user }) {
-    return Request.post(`/refs/${refId}/users`).send({ user_id: user });
-}
-
-export function editUser({ refId, userId, update }) {
-    return Request.patch(`/refs/${refId}/users/${userId}`).send(update);
-}
-
-export function addGroup({ refId, group }) {
-    return Request.post(`/refs/${refId}/groups`).send({ group_id: group });
-}
-
-export function editGroup({ refId, groupId, update }) {
-    return Request.patch(`/refs/${refId}/groups/${groupId}`).send(update);
-}
-
-export function checkUpdates({ refId }) {
-    return Request.get(`/refs/${refId}/release`);
-}
-
-export function updateRemote({ refId }) {
-    return Request.post(`/refs/${refId}/updates`).send({});
 }
 
 /**
