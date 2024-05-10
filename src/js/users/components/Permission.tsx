45718bc78588b6d90ecea8e320d2b36792b9e2e2
Jason Thomo <jasonthomo4@gmail.com>
2024-05-10 14:51:16 -0700

    style: create PermissionItemProps type

--- src/js/users/components/Permission.tsx
+++ src/js/users/components/Permission.tsx
@@ -11,12 +11,19 @@ const StyledPermissionItem = styled(BoxGroupSection)`
     display: flex;
 `;
 
+type PermissionItemProps = {
+    /* Name of the permission */
+    permission: string;
+    /* Indicates if the permission is granted */
+    value: boolean;
+};
+
 /**
-A condensed permission item for use in a list of user permissions
+ * Permission item with an icon indicating its status.
  * @prop permission - name of the permission.
  * @prop value - indicates if the permission is granted.
  */
-export function PermissionItem({ permission, value }: { permission: string; value: boolean }) {
+export function PermissionItem({ permission, value }: PermissionItemProps) {
     return (
         <StyledPermissionItem aria-label={`${permission}:${value}`}>
             <StyledPermissionIcon name={value ? "check" : "times"} color={value ? "green" : "red"} fixedWidth />
