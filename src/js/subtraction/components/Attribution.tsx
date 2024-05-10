e606bd4ea9b18ece6e3f74684b7bf7050ec368b4
Jason Thomo <jasonthomo4@gmail.com>
2024-05-10 10:06:31 -0700

    refactor: add typing and docstrings

--- src/js/subtraction/components/Attribution.tsx
+++ src/js/subtraction/components/Attribution.tsx
@@ -3,6 +3,9 @@ import styled from "styled-components";
 import { getColor, getFontSize, theme } from "../../app/theme";
 import { Attribution, AttributionWithName } from "../../base";
 
+/**
+ * Styled component for the attribution when none found.
+ */
 const StyledNoneFoundAttribution = styled.div`
     color: ${getColor({ color: "grey", theme })};
     font-size: ${getFontSize("sm")};
@@ -11,7 +14,23 @@ const StyledNoneFoundAttribution = styled.div`
     top: 50%;
 `;
 
-export function SubtractionAttribution({ handle, time }) {
+/**
+ * Props interface for the SubtractionAttribution component.
+ * @prop {string} handle - The user handle.
+ * @prop {string} time - The time of the subtraction.
+ */
+interface SubtractionAttributionProps {
+    handle: string;
+    time?: string;
+}
+
+/**
+ * Component for rendering attribution with user and time.
+ * If only user is provided, renders without time.
+ * If neither user nor time is provided, renders default message.
+ * @param {SubtractionAttributionProps} props - The component props.
+ */
+export function SubtractionAttribution({ handle, time }: SubtractionAttributionProps): JSX.Element {
     if (handle) {
         if (time) {
             return <Attribution user={handle} time={time} />;
