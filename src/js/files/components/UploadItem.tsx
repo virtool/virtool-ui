0e5f5b7dd1f0e62ab88275d1ae0e728f93c973d9
Jason Thomo <jthomo@uvic.ca>
2024-05-14 13:53:05 -0700

    refactor: use UploadItemTitleProps type and update docstrings

--- src/js/files/components/UploadItem.tsx
+++ src/js/files/components/UploadItem.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-import { getBorder, getColor, getFontWeight } from "../../app/theme";
+import { getBorder, getColor, getFontWeight, theme } from "../../app/theme";
 import { Icon, Loader, ProgressBarAffixed } from "../../base";
 import { byteSize } from "../../utils/utils";
 
 /**
- * Styled component for the upload item container.
+ * Container for the upload item.
  */
 const StyledUploadItem = styled.div`
     padding: 0;
@@ -17,14 +17,14 @@ const StyledUploadItem = styled.div`
 `;
 
 type UploadItemTitleProps = {
-     /** whether the upload failed. */
-     failed: boolean
+    /** Whether the upload failed */
+    failed: boolean
 }
 
 /** 
-Styled component for the title section of the upload item. 
-*/
-const UploadItemTitle = styled.div<{ failed: boolean }>`
+ * Container for the title section of the upload item.
+ */
+const UploadItemTitle = styled.div<UploadItemTitleProps>`
     justify-content: space-between;
     display: flex;
     padding: 15px 15px 10px;
@@ -35,7 +35,7 @@ const UploadItemTitle = styled.div<{ failed: boolean }>`
     }
     span:last-child {
         margin-left: auto;
-        color: ${props => (props.failed ? getColor({ theme: props.theme, color: "red" }) : "inherit")};
+        color: ${props => (props.failed ? getColor({ theme: theme, color: "red" }) : "inherit")};
     }
     i.fa-times {
         font-size: 20px;
