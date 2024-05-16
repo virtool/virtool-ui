d613283a4aee34a7d54abd97ea9617075d10f3fe
Rastin Rashidi <rastinrashidi@gmail.com>
2024-05-16 13:49:07 -0700

    chore: remove unused redux

--- src/js/references/actions.js
+++
@@ -1,4 +0,0 @@
-import { GET_REFERENCE } from "@app/actionTypes";
-import { createAction } from "@reduxjs/toolkit";
-
-export const getReference = createAction(GET_REFERENCE.REQUESTED, refId => ({ payload: { refId } }));
