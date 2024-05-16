01c484d1e11caf018c44ff376640430dbd83bc3b
Rastin Rashidi <rastinrashidi@gmail.com>
2024-05-16 13:42:33 -0700

    refactor: update API key dialogue data handling

---
+++ src/js/account/components/API/APIPermissions.tsx
@@ -0,0 +1,52 @@
+import { useFetchAccount } from "@account/queries";
+import { AdministratorRoles } from "@administration/types";
+import { AdministratorPermissions, hasSufficientAdminRole } from "@administration/utils";
+import { BoxGroup, BoxGroupSection, Checkbox } from "@base";
+import { Permissions } from "@groups/types";
+import { map, sortBy } from "lodash-es";
+import React from "react";
+
+type APIPermissionsProps = {
+    className?: string;
+    keyPermissions: Permissions;
+    /** Callback function to handle permission selection */
+    onChange: (permissions: Permissions) => void;
+};
+
+/**
+ * Manages permissions for creating/updating an API
+ */
+export default function APIPermissions({ className, keyPermissions, onChange }: APIPermissionsProps) {
+    const { data, isLoading } = useFetchAccount();
+
+    if (isLoading) {
+        return null;
+    }
+
+    const permissions = map(keyPermissions, (value, key) => ({
+        name: key,
+        allowed: value,
+    }));
+
+    const rowComponents = map(sortBy(permissions, "name"), permission => {
+        const disabled =
+            !hasSufficientAdminRole(
+                AdministratorPermissions[permission.name] as AdministratorRoles,
+                data.administrator_role,
+            ) && !data.permissions[permission.name];
+
+        return (
+            <BoxGroupSection key={permission.name} disabled={disabled}>
+                <Checkbox
+                    checked={permission.allowed}
+                    onClick={
+                        disabled ? null : () => onChange({ ...keyPermissions, [permission.name]: !permission.allowed })
+                    }
+                    label={permission.name}
+                />
+            </BoxGroupSection>
+        );
+    });
+
+    return <BoxGroup className={className}>{rowComponents}</BoxGroup>;
+}
--- src/js/account/queries.ts
+++ src/js/account/queries.ts
@@ -1,12 +1,15 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchAccount } from "./api";
-import { Account } from "./types";
+import { ErrorResponse } from "@/types/types";
+import { Permissions } from "@groups/types";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { createAPIKey, fetchAccount, getAPIKeys, removeAPIKey, updateAPIKey } from "./api";
+import { Account, APIKeyMinimal } from "./types";
 
 /**
  * Factory object for generating account query keys
  */
 export const accountKeys = {
     all: () => ["account"],
+    details: () => ["account", "details"] as const,
 };
 
 /**
@@ -14,6 +17,59 @@ export const accountKeys = {
  *
  * @returns UseQueryResult object containing the account data
  */
-export const useFetchAccount = () => {
+export function useFetchAccount() {
     return useQuery<Account>(accountKeys.all(), () => fetchAccount());
-};
+}
+
+/**
+ * Fetches the API keys for the current user
+ *
+ * @returns A list of API keys for the current user
+ */
+export function useFetchAPIKeys() {
+    return useQuery<APIKeyMinimal[]>(accountKeys.details(), () => getAPIKeys());
+}
+
+/**
+ * Initializes a mutator for creating a new API key
+ *
+ * @returns A mutator for creating a new API key
+ */
+export function useCreateAPIKey() {
+    return useMutation<APIKeyMinimal, ErrorResponse, { name: string; permissions: Permissions }>(
+        ({ name, permissions }) => createAPIKey(name, permissions),
+    );
+}
+
+/**
+ * Initializes a mutator for updating an API key
+ *
+ * @returns A mutator for updating an API key
+ */
+export function useUpdateAPIKey() {
+    const queryClient = useQueryClient();
+
+    return useMutation<APIKeyMinimal, ErrorResponse, { keyId: string; permissions: Permissions }>(
+        ({ keyId, permissions }) => updateAPIKey(keyId, permissions),
+        {
+            onSuccess: () => {
+                queryClient.invalidateQueries(accountKeys.all());
+            },
+        },
+    );
+}
+
+/**
+ * Initializes a mutator for removing an API key
+ *
+ * @returns A mutator for removing an API key
+ */
+export function useRemoveAPIKey() {
+    const queryClient = useQueryClient();
+
+    return useMutation<null, ErrorResponse, { keyId: string }>(({ keyId }) => removeAPIKey(keyId), {
+        onSuccess: () => {
+            queryClient.invalidateQueries(accountKeys.all());
+        },
+    });
+}
