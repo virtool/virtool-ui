ffa917cc9bd11a521401dfd3763e553898888b15
Rastin Rashidi <rastinrashidi@gmail.com>
2024-06-07 12:06:44 -0700

    test: fix deepsource issues with test

--- src/js/base/__tests__/RelativeTime.test.jsx
+++ src/js/base/__tests__/RelativeTime.test.jsx
@@ -1,8 +1,8 @@
+import { RelativeTime } from "@base";
 import { act, screen } from "@testing-library/react";
 import React from "react";
 import { afterAll, beforeAll, describe, expect, it, vi } from "vitest";
 import { renderWithProviders } from "../../../tests/setupTests";
-import { RelativeTime } from "../RelativeTime";
 
 const fakeTime = "2019-02-10T17:11:00.000000Z";
 const RealDate = Date;
@@ -13,23 +13,23 @@ describe("<RelativeTime />", () => {
         Date.now.mockReturnValue(new Date("2019-04-22T10:20:30Z"));
     });
 
-    it("should render 2 months ago", async () => {
+    it("should render 2 months ago", () => {
         renderWithProviders(<RelativeTime time={fakeTime} />);
         expect(screen.getByText("2 months ago")).toHaveTextContent("2 months ago");
     });
 
-    it("should re-render when time prop changes", async () => {
+    it("should re-render when time prop changes", () => {
         const { rerender } = renderWithProviders(<RelativeTime time={fakeTime} />);
         expect(screen.getByText("2 months ago")).toBeInTheDocument();
 
         // Add one month to the time prop.
         rerender(<RelativeTime time="2019-01-10T12:21:00.000000Z" />);
 
-        expect(await screen.queryByText("2 months ago")).toBeNull();
-        expect(await screen.getByText("3 months ago")).toBeInTheDocument();
+        expect(screen.queryByText("2 months ago")).toBeNull();
+        expect(screen.getByText("3 months ago")).toBeInTheDocument();
     });
 
-    it("should re-render when time string changes", async () => {
+    it("should re-render when time string changes", () => {
         vi.useFakeTimers().setSystemTime(new Date("2019-04-22T10:20:30Z"));
 
         // Render with time that is only 10 seconds before the current (mocked) time.
