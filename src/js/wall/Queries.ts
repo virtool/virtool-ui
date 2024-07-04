2c807bfc54211edd0b9eb7641207cc7ab8918a97
Jason Thomo <jasonthomo4@gmail.com>
2024-07-04 12:09:41 -0700

    chore: create mutation for password reset

--- src/js/wall/Queries.ts
+++ src/js/wall/Queries.ts
@@ -1,4 +1,4 @@
-import { login } from "@/account/api";
+import { login, resetPassword } from "@/account/api";
 import { ErrorResponse } from "@/types/types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Response } from "superagent";
@@ -21,6 +21,24 @@ export function useLoginMutation() {
 
     return useMutation<Response, ErrorResponse, { username: string; password: string; remember: boolean }>(
         ({ username, password, remember }) => login({ username, password, remember }),
+        {
+            onSuccess: () => {
+                queryClient.invalidateQueries(accountKeys.all());
+            },
+        },
+    );
+}
+
+/**
+ * Initializes a mutator for sending a password reset request to the API.
+ *
+ * @returns A mutator for sending a password reset request to the API.
+ */
+export function useResetPasswordMutation() {
+    const queryClient = useQueryClient();
+
+    return useMutation<Response, ErrorResponse, { password: string; resetCode: string }>(
+        ({ password, resetCode }) => resetPassword({ password, resetCode }),
         {
             onSuccess: () => {
                 queryClient.invalidateQueries(accountKeys.all());
