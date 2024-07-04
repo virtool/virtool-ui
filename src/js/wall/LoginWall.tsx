1fa33877e53fa795652bba80bde3b38ecf6a9d75
Jason Thomo <jasonthomo4@gmail.com>
2024-07-04 12:13:02 -0700

    refactor: strip out redux from LoginWall and use local state for reset code

--- src/js/wall/LoginWall.tsx
+++ src/js/wall/LoginWall.tsx
@@ -1,31 +1,24 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { useState } from "react";
 import { WallContainer, WallDialog, WallLoginContainer } from "./Container";
 import LoginForm from "./LoginForm";
 import ResetForm from "./ResetForm";
 
-type LoginWallProps = {
-    /** Indicates if the user needs to reset their password. */
-    reset: boolean;
-};
+function LoginWall() {
+    const [resetCode, setResetCode] = useState<string | null>(null);
 
-/** Renders either ResetForm or LoginForm based on the reset state. */
-export function LoginWall({ reset }: LoginWallProps) {
     return (
         <WallContainer>
             <WallDialog>
                 <WallLoginContainer>
-                    {reset ? <ResetForm /> : <LoginForm error="Invalid username or password" />}
+                    {resetCode ? (
+                        <ResetForm error="Invalid password" resetCode={resetCode} />
+                    ) : (
+                        <LoginForm error="Invalid username or password" setResetCode={setResetCode} />
+                    )}
                 </WallLoginContainer>
             </WallDialog>
         </WallContainer>
     );
 }
 
-export function mapStateToProps(state) {
-    return {
-        reset: state.app.reset,
-    };
-}
-
-export default connect(mapStateToProps)(LoginWall);
+export default LoginWall;
