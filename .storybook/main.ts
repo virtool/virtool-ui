9fcef31b956e97ae207fadfb66318ebfbb959705
Rastin Rashidi <rastinrashidi@gmail.com>
2024-07-16 16:08:49 -0700

    fix: reinstall storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
     stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+
     addons: [
         "@storybook/addon-links",
         "@storybook/addon-essentials",
@@ -9,11 +10,17 @@ const config: StorybookConfig = {
         "@storybook/addon-webpack5-compiler-babel",
         "@chromatic-com/storybook",
     ],
+
     framework: {
         name: "@storybook/react-vite",
         options: {},
     },
+
     docs: {},
+
+    typescript: {
+        reactDocgen: "react-docgen-typescript"
+    }
 };
 
 export default config;
--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -27,3 +27,4 @@ export const parameters = {
         },
     },
 };
+export const tags = ["autodocs"];
