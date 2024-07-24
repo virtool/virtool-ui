4a833f8b29e4811b38a40b555f89597d408a87b0
Rastin Rashidi <rastinrashidi@gmail.com>
2024-07-24 14:47:22 -0700

    feat: show warning when viewing reads in pathoscope

--- src/js/analyses/components/Pathoscope/PathoscopeViewer.tsx
+++ src/js/analyses/components/Pathoscope/PathoscopeViewer.tsx
@@ -1,6 +1,8 @@
 import { PathoscopeViewerScroller } from "@/analyses/components/Pathoscope/PathoscopeViewScroller";
 import { FormattedPathoscopeAnalysis } from "@/analyses/types";
+import { Alert } from "@base";
 import { Sample } from "@samples/types";
+import { useUrlSearchParams } from "@utils/hooks";
 import React from "react";
 import { AnalysisMapping } from "./AnalysisMapping";
 import { PathoscopeList } from "./PathoscopeList";
@@ -15,10 +17,24 @@ type PathoscopeViewerProps = {
 
 /** Detailed breakdown of the results of a pathoscope analysis */
 export function PathoscopeViewer({ detail, sample }: PathoscopeViewerProps) {
+    const [showReads] = useUrlSearchParams<boolean>("reads");
+
     return (
         <>
             <AnalysisMapping detail={detail} totalReads={sample.quality.count} />
             <PathoscopeToolbar analysisId={detail.id} />
+            {showReads && (
+                <Alert color="orange" level>
+                    <div>
+                        <p className="font-bold">Read Numbers are not realistic.</p>
+                        <p>
+                            Read numbers are arbitrarily calculated using weight × total mapped reads and are not
+                            representative of actual numbers of reads mapped to viruses.
+                        </p>
+                        <p>Read numbers are shown only for continuity.</p>
+                    </div>
+                </Alert>
+            )}
             <PathoscopeList detail={detail} sample={sample} />
             <PathoscopeViewerScroller />
         </>
