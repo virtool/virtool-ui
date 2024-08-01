a3623d7491d51f2a057947deff09f081cfa7902f
Rastin Rashidi <rastinrashidi@gmail.com>
2024-08-01 15:09:56 -0700

    refactor: reimplement markdown in tailwind

--- src/js/base/Markdown.tsx
+++ src/js/base/Markdown.tsx
@@ -1,14 +1,7 @@
+import { cn } from "@utils/utils";
 import { marked } from "marked";
 import React from "react";
-import styled from "styled-components";
 import { NoneFound } from "./NoneFound";
-
-const StyledMarkdown = styled.div`
-    overflow-y: scroll;
-    max-height: 375px;
-    margin-bottom: 0;
-    padding: 10px 15px;
-`;
 
 type MarkdownProps = {
     markdown?: string;
@@ -21,12 +14,17 @@ export function Markdown({ markdown = "" }: MarkdownProps) {
     });
 
     if (markdown) {
-        return <StyledMarkdown dangerouslySetInnerHTML={{ __html: marked.parse(markdown) as string }} />;
+        return (
+            <div
+                className={cn("overflow-y-scroll", "max-h-96", "mb-0", "py-2.5", "px-4")}
+                dangerouslySetInnerHTML={{ __html: marked.parse(markdown) as string }}
+            />
+        );
     }
 
     return (
-        <StyledMarkdown>
+        <div className={cn("overflow-y-scroll", "max-h-96", "mb-0", "py-2.5", "px-4")}>
             <NoneFound noun="notes" />
-        </StyledMarkdown>
+        </div>
     );
 }
