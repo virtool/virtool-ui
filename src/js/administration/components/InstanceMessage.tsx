b8c5d0ffa06b18041fbb60a215f49d1fd650fa27
Rastin Rashidi <rastinrashidi@gmail.com>
2024-08-08 12:09:10 -0700

    fix: remove unused styling

--- src/js/administration/components/InstanceMessage.tsx
+++ src/js/administration/components/InstanceMessage.tsx
@@ -1,29 +1,8 @@
-import { getFontSize } from "@app/theme";
-import {
-    BoxGroup,
-    BoxGroupHeader,
-    BoxGroupSection,
-    BoxTitle,
-    InputGroup,
-    InputLabel,
-    InputSimple,
-    SaveButton,
-} from "@base";
+import { BoxGroup, BoxGroupHeader, BoxGroupSection, InputGroup, InputLabel, InputSimple, SaveButton } from "@base";
 import { useSetMessage } from "@message/queries";
 import { Message } from "@message/types";
 import React from "react";
 import { useForm } from "react-hook-form";
-import styled from "styled-components";
-
-const InstanceMessageTitle = styled(BoxTitle)`
-    font-size: ${getFontSize("lg")};
-    margin-bottom: 8px;
-`;
-
-const InstanceMessageSubtitle = styled.p`
-    color: ${props => props.theme.color.greyDarkest};
-    margin: 0 0 15px;
-`;
 
 type InstanceMessageProps = {
     message: Message;
