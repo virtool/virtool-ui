fc17412f07af647aef88e64033fead57c96b47d3
Rastin Rashidi <rastinrashidi@gmail.com>
2024-08-08 12:08:14 -0700

    refactor: update administrator settings styling

--- src/js/administration/components/InstanceMessage.tsx
+++ src/js/administration/components/InstanceMessage.tsx
@@ -1,5 +1,14 @@
 import { getFontSize } from "@app/theme";
-import { Box, BoxTitle, InputGroup, InputLabel, InputSimple, SaveButton } from "@base";
+import {
+    BoxGroup,
+    BoxGroupHeader,
+    BoxGroupSection,
+    BoxTitle,
+    InputGroup,
+    InputLabel,
+    InputSimple,
+    SaveButton,
+} from "@base";
 import { useSetMessage } from "@message/queries";
 import { Message } from "@message/types";
 import React from "react";
@@ -30,16 +39,20 @@ export default function InstanceMessage({ message }: InstanceMessageProps) {
     const mutation = useSetMessage();
 
     return (
-        <Box>
-            <InstanceMessageTitle>Instance Message</InstanceMessageTitle>
-            <InstanceMessageSubtitle>Display a message to all users above the navigation bar.</InstanceMessageSubtitle>
-            <form onSubmit={handleSubmit(({ message }) => mutation.mutate({ message }))}>
-                <InputGroup>
-                    <InputLabel htmlFor="message">Message</InputLabel>
-                    <InputSimple id="message" {...register("message")} />
-                </InputGroup>
-                <SaveButton />
-            </form>
-        </Box>
+        <BoxGroup>
+            <BoxGroupHeader>
+                <h2>Instance Message</h2>
+                <p>Display a message to all users above the navigation bar.</p>
+            </BoxGroupHeader>
+            <BoxGroupSection>
+                <form onSubmit={handleSubmit(({ message }) => mutation.mutate({ message }))}>
+                    <InputGroup>
+                        <InputLabel htmlFor="message">Message</InputLabel>
+                        <InputSimple id="message" {...register("message")} />
+                    </InputGroup>
+                    <SaveButton />
+                </form>
+            </BoxGroupSection>
+        </BoxGroup>
     );
 }
