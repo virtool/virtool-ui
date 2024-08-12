5f2a7a9322d318400040c2ee8057905b18ddc493
Rastin Rashidi <rastinrashidi@gmail.com>
2024-08-12 13:01:02 -0700

    refactor: rework dropdown meu content to use tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 export default {
     content: ["./src/js/**/*.{html,js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            keyframes: {
+                slideDown: {
+                    "0%": { opacity: "0", transform: "translateY(-10px)" },
+                    "100%": { opacity: "1", transform: "translateY(0)" },
+                },
+            },
+            animation: {
+                slideDown: "slideDown 100ms ease-in",
+            },
+        },
     },
     plugins: [],
 };
