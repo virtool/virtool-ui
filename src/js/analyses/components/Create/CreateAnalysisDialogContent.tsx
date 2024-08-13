a4bec0ade6b70f43be3546925a03bbb30ace20f2
Rastin Rashidi <rastinrashidi@gmail.com>
2024-08-13 11:21:10 -0700

    refactor: rework dialogs to use tailwind

--- src/js/analyses/components/Create/CreateAnalysisDialogContent.tsx
+++ src/js/analyses/components/Create/CreateAnalysisDialogContent.tsx
@@ -1,29 +1,14 @@
 import { DialogContent } from "@base";
-import styled, { keyframes } from "styled-components";
+import { cn } from "@utils/utils";
+import React from "react";
 
-const createAnalysisOpen = keyframes`
-  from {
-    opacity: 0;
-    transform: translate(-50%, -2%) scale(0.96);
-  }
-  to {
-    opacity: 1;
-    transform: translate(-50%, 0%) scale(1);
-  }
-`;
-
-export const CreateAnalysisDialogContent = styled(DialogContent)`
-    animation: ${createAnalysisOpen} 150ms cubic-bezier(0.16, 1, 0.3, 1);
-    max-height: 90vh;
-    overflow: auto;
-    position: fixed;
-    top: 5%;
-    transform: translate(-50%, 0%);
-    width: 700px;
-
-    form {
-        display: flex;
-        flex-direction: column;
-        height: 100%;
-    }
-`;
+/**
+ * A styled dialog content for use in an analysis creation dialog
+ */
+export function CreateAnalysisDialogContent({ children }) {
+    return (
+        <DialogContent className={cn("max-h-[90vh]", "overflow-auto", "top-[45%]", "w-[700px]")}>
+            <div className={cn("flex", "flex-col", "h-full")}>{children}</div>
+        </DialogContent>
+    );
+}
