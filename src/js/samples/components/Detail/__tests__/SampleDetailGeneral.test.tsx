90557bc5281d38eb605be9fc1f94302991a2bd6e
Jason Thomo <jasonthomo4@gmail.com>
2024-08-26 12:52:28 -0700

    chore: deepsource fix

--- src/js/samples/components/Detail/__tests__/SampleDetailGeneral.test.tsx
+++ src/js/samples/components/Detail/__tests__/SampleDetailGeneral.test.tsx
@@ -7,7 +7,6 @@ import React from "react";
 import { beforeEach, describe, expect, it } from "vitest";
 
 describe("<SampleDetailGeneral />", () => {
-    let props;
     let sample;
 
     beforeEach(() => {
