6e66af02621356d7f89b937d69f0488b80198d90
Jason Thomo <jasonthomo4@gmail.com>
2024-08-28 03:21:32 -0700

    refactor: use v6 relative links, remove history & match prop

--- src/js/subtraction/components/Subtraction.tsx
+++ src/js/subtraction/components/Subtraction.tsx
@@ -1,6 +1,7 @@
 import { Container, ContainerNarrow } from "@base";
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Switch } from "react-router-dom";
+import { CompatRoute } from "react-router-dom-v5-compat";
 import SubtractionDetail from "./Detail/SubtractionDetail";
 import { SubtractionFileManager } from "./SubtractionFileManager";
 import SubtractionList from "./SubtractionList";
@@ -13,9 +14,9 @@ export default function Subtraction() {
         <Container>
             <ContainerNarrow>
                 <Switch>
-                    <Route path="/subtractions" component={SubtractionList} exact />
-                    <Route path="/subtractions/files" component={SubtractionFileManager} />
-                    <Route path="/subtractions/:subtractionId" component={SubtractionDetail} />
+                    <CompatRoute path="/subtractions" component={SubtractionList} exact />
+                    <CompatRoute path="/subtractions/files" component={SubtractionFileManager} />
+                    <CompatRoute path="/subtractions/:subtractionId" component={SubtractionDetail} />
                 </Switch>
             </ContainerNarrow>
         </Container>
