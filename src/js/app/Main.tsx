83ea9d41e4b9975c6461500b56d5e5270c87d208
Jason Thomo <jasonthomo4@gmail.com>
2024-08-29 10:44:13 -0700

    refactor: upgrade account to react-router v6 API

--- src/js/app/Main.tsx
+++ src/js/app/Main.tsx
@@ -87,7 +87,7 @@ export default function Main() {
                         <Route path="/hmm" component={HMM} />
                         <CompatRoute path="/subtractions" component={Subtraction} />
                         <Route path="/administration" component={Administration} />
-                        <Route path="/account" component={Account} />
+                        <CompatRoute path="/account" component={Account} />
                         <Route path="/ml" component={ML} />
                     </Switch>
                 </Suspense>
