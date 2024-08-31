4f50901e6c9b0c2294fc7d4b2add02df227e0779
Rastin Rashidi <rastinrashidi@gmail.com>
2024-08-31 10:02:05 -0700

    chore: upgrade jobs to v6 api

--- src/js/jobs/components/Jobs.tsx
+++ src/js/jobs/components/Jobs.tsx
@@ -1,6 +1,6 @@
 import { Container } from "@base";
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom-v5-compat";
 import JobDetail from "./JobDetail";
 import JobsList from "./JobList";
 
@@ -10,10 +10,10 @@ import JobsList from "./JobList";
 export default function Jobs() {
     return (
         <Container>
-            <Switch>
-                <Route path="/jobs" component={JobsList} exact />
-                <Route path="/jobs/:jobId" component={JobDetail} />
-            </Switch>
+            <Routes>
+                <Route path="/jobs" element={<JobsList />} />
+                <Route path="/jobs/:jobId" element={<JobDetail />} />
+            </Routes>
         </Container>
     );
 }
