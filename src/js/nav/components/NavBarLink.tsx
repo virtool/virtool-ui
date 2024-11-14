5b2018724ebd045dbc6c1a3101028c45456dd258
Reece Hoffmann <59776400+ReeceHoffmann@users.noreply.github.com>
2024-11-14 09:33:59 -0800

    fix: ensure internal links highlight when on the matching page

--- src/js/nav/components/NavBarLink.tsx
+++ src/js/nav/components/NavBarLink.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@utils/utils";
 import React from "react";
 import { Link } from "@base";
+import { useMatchPartialPath } from "@utils/hooks";
 
 const navBarkLinkClassName = cn(
     "text-white",
@@ -15,11 +16,12 @@ const navBarkLinkClassName = cn(
 );
 
 export function NavBarLink({ children, to }) {
+    const isActive = useMatchPartialPath(to);
     return (
         <Link
             to={to}
             className={active =>
-                active
+                isActive || active
                     ? cn(navBarkLinkClassName, "text-white", "bg-primary-dark", "hover:text-white")
                     : navBarkLinkClassName
             }
