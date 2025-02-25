39e8f2533bd51fd9acb8e6a5cdd073604b3dfe11
Ian <igboyes@gmail.com>
2025-02-25 10:51:53 -0800

    chore: run prettier elsewhere

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,8 +18,14 @@ export default {
                     to: { opacity: "1" },
                 },
                 contentShow: {
-                    from: { opacity: "0", transform: "translate(-50%, -48%) scale(0.96)" },
-                    to: { opacity: "1", transform: "translate(-50%, -50%) scale(1)" },
+                    from: {
+                        opacity: "0",
+                        transform: "translate(-50%, -48%) scale(0.96)",
+                    },
+                    to: {
+                        opacity: "1",
+                        transform: "translate(-50%, -50%) scale(1)",
+                    },
                 },
                 slideDown: {
                     "0%": { opacity: "0", transform: "translateY(-10px)" },
