9e0c93d66d5b2f5ae1d46e498d5cf76b1c4c68aa
Ian <igboyes@gmail.com>
2025-03-07 08:17:01 -0800

    refactor: remove non-default `BoxTitle` export

--- src/js/base/BoxTitle.ts
+++ src/js/base/BoxTitle.ts
@@ -4,7 +4,7 @@ type BoxTitleProps = {
     theme: DefaultTheme;
 };
 
-export const BoxTitle = styled.h1<BoxTitleProps>`
+const BoxTitle = styled.h1<BoxTitleProps>`
     font-size: ${(props) => props.theme.fontSize.md};
     font-weight: ${(props) => props.theme.fontWeight.thick};
     margin: 5px 0 15px 0;
