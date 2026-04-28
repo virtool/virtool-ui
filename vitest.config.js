c6e88bbeb9e92668a27922c414f48f1c7aa696f1
Ian Boyes <igboyes@gmail.com>
2026-04-28 10:17:57 -0700

    feat: update dependencies

--- vitest.config.js
+++ vitest.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     test: {
         globals: true,
         environment: "jsdom",
-        setupFiles: ["./src/tests/setup.tsx"],
+        setupFiles: ["./tests/setup.tsx"],
         silent: false,
     },
 });
